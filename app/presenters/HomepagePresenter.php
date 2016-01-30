174e05b7a9c36fe51ccd0094ba1efa5de0cbe3f5
Filip Procházka <filip@prochazka.su>
2016-01-30 12:00:24 +0100

    example form for pushing messages

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -2,9 +2,44 @@
 
 namespace App\Presenters;
 
+use App\EmailQueue;
 use Nette;
+use Nette\Application\UI\Form;
 
 
 class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+
+    /**
+     * @var EmailQueue
+     * @inject
+     */
+    public $emailQueue;
+
+
+    /**
+     * @return Form
+     */
+    protected function createComponentEmailForm()
+    {
+        $form = new Form();
+        $form->addText('from', 'Od');
+        $form->addText('to', 'Komu');
+        $form->addTextArea('body', 'Zpráva');
+
+        $form->addSubmit("send", "Odeslat");
+        $form->onSuccess[] = function (Form $form, $values) {
+            $this->emailQueue->publish(
+                $values->to,
+                $values->from,
+                $values->body
+            );
+
+            $this->flashMessage('Yay!');
+            $this->redirect('this');
+        };
+
+        return $form;
+    }
+
 }
