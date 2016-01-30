353799b62e4cd6f1db9a74bda0a9daeea6ca6e4a
Filip Procházka <filip@prochazka.su>
2016-01-30 11:16:12 +0100

    cleanup

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types = 1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -6,6 +6,7 @@ $configurator = new Nette\Configurator;
 
 //$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
 $configurator->enableDebugger(__DIR__ . '/../log');
+Kdyby\Console\DI\BootstrapHelper::setupMode($configurator);
 
 $configurator->setTempDirectory(__DIR__ . '/../temp');
 
@@ -16,6 +17,4 @@ $configurator->createRobotLoader()
 $configurator->addConfig(__DIR__ . '/config/config.neon');
 $configurator->addConfig(__DIR__ . '/config/config.local.neon');
 
-$container = $configurator->createContainer();
-
-return $container;
+return $configurator->createContainer();
--- www/index.php
+++ www/index.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types = 1);
 
 $container = require __DIR__ . '/../app/bootstrap.php';
-$container->getByType('Nette\Application\Application')->run();
+$container->getByType(Nette\Application\Application::class)->run();
